a98313f1668f290d58bb45bcb86cd0c4a23f0f14
Tim Mitra <twitter@it-guy.com>
2022-04-05 10:08:56 -0400

    Initial Commit

---
+++ Storm Viewer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Tim Mitra on 4/5/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
