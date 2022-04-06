5cb7d09920ef35aebd59cb60f7437de41459cc49
Tim Mitra <tim@it-guy.com>
2022-04-06 00:25:03 -0400

    add picture names to table

--- Storm Viewer/SourceViewController.swift
+++ Storm Viewer/SourceViewController.swift
@@ -33,7 +33,7 @@ class SourceViewController: NSViewController, NSTableViewDelegate, NSTableViewDa
     
   func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
     guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
-    vw.textField?.stringValue = "Hello, world!"
+    vw.textField?.stringValue = pictures[row]
     
     return vw
   }
