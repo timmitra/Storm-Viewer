b7e459e58e4833fbb64d76b185bd16d7ac771fcb
Tim Mitra <tim@it-guy.com>
2022-04-06 00:09:41 -0400

    return 100 tableView rows

--- Storm Viewer/SourceViewController.swift
+++ Storm Viewer/SourceViewController.swift
@@ -7,12 +7,16 @@
 
 import Cocoa
 
-class SourceViewController: NSViewController {
+class SourceViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
   @IBOutlet var tableView: NSTableView!
   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
+  
+  func numberOfRows(in tableView: NSTableView) -> Int {
+    return 100
+  }
     
 }
