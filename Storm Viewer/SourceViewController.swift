445a31f4373f42f9309d135af7e10a54c4d99952
Tim Mitra <tim@it-guy.com>
2022-04-06 00:33:47 -0400

    show select image

--- Storm Viewer/SourceViewController.swift
+++ Storm Viewer/SourceViewController.swift
@@ -37,4 +37,13 @@ class SourceViewController: NSViewController, NSTableViewDelegate, NSTableViewDa
     
     return vw
   }
+  
+  func tableViewSelectionDidChange(_ notification: Notification) {
+    guard tableView.selectedRow != -1 else { return }
+    guard let splitVC = parent as? NSSplitViewController else { return }
+    
+    if let detail = splitVC.children[1] as? DetailViewController {
+      detail.imageSelected(name: pictures[tableView.selectedRow])
+    }
+  }
 }
